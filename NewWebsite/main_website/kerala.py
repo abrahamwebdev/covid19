ce421dd342adc402bd47bc94100153a76f18acab
abraham <agthankachan007@gmail.com>
2021-06-28 12:22:11 +0530

    2

--- NewWebsite/main_website/kerala.py
+++ NewWebsite/main_website/kerala.py
@@ -1,4 +1,13 @@
 import pickle
+import datetime
+import pandas as pd
+
 # pkl_path = "D:\covid19\NewWebsite\main_website\Kerala.pkl"
+date = list()
+dat = '2021-6-11'
+date.append([dat])
+date = pd.DataFrame(date)
+date.columns = ['ds']
+date['ds'] = to_datetime(date['ds'])
 with open('Kerala.pkl', 'rb') as keralapickle:
     model = pickle.load(keralapickle)
