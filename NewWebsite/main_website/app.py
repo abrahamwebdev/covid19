7363a1121e9d03e681610f1af2f7cf3f7c52f225
abraham <agthankachan007@gmail.com>
2021-06-29 01:01:57 +0530

    app kerala 1

--- NewWebsite/main_website/app.py
+++ NewWebsite/main_website/app.py
@@ -1,4 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, redirect,url_for, render_template, request
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import datetime
+import pickle
+from pandas import to_datetime
 
 app = Flask(__name__)
 
@@ -7,9 +13,22 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
-@app.route("/kerala")
+@app.route("/kerala" ,methods=["POST", "GET"])
 def kerala():
-    return render_template("kerala.html")
+    if request.method == "POST":
+        date = list()
+        dat=request.form["nm"]
+        date.append([dat])
+        date = pd.DataFrame(date)
+        date.columns = ['ds']
+        date['ds'] = to_datetime(date['ds'])
+        with open('Kerala.pkl', 'rb') as f:
+            model = pickle.load(f)
+        forecast = model.predict(date)
+        x=forecast.iloc[ : ,3].values
+
+    else:
+        return render_template("kerala.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
