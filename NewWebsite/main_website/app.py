7dcc569c1e3b072d7511643bf31c7548fe9e2c08
abraham <agthankachan007@gmail.com>
2021-06-29 12:18:34 +0530

    all states

--- NewWebsite/main_website/app.py
+++ NewWebsite/main_website/app.py
@@ -22,7 +22,7 @@ def kerala():
         date = pd.DataFrame(date)
         date.columns = ['ds']
         date['ds'] = to_datetime(date['ds'])
-        with open('Kerala.pkl', 'rb') as f:
+        with open('models/Kerala.pkl', 'rb') as f:
             model = pickle.load(f)
         forecast = model.predict(date)
         x=forecast.iloc[ : ,3].values
@@ -31,7 +31,7 @@ def kerala():
     else:
         return render_template("state.html")
 
-@app.route("/tn" ,methods=["POST", "GET"])
+@app.route("/tamilnadu" ,methods=["POST", "GET"])
 def tn():
     if request.method == "POST":
         date = list()
@@ -40,7 +40,187 @@ def tn():
         date = pd.DataFrame(date)
         date.columns = ['ds']
         date['ds'] = to_datetime(date['ds'])
-        with open('TamilNadu.pkl', 'rb') as f:
+        with open('models/TamilNadu.pkl', 'rb') as f:
+            model = pickle.load(f)
+        forecast = model.predict(date)
+        x=forecast.iloc[ : ,3].values
+        return redirect(url_for("predict",pred=x))
+
+    else:
+        return render_template("state.html")
+
+@app.route("/karnataka" ,methods=["POST", "GET"])
+def kn():
+    if request.method == "POST":
+        date = list()
+        dat=request.form["nm"]
+        date.append([dat])
+        date = pd.DataFrame(date)
+        date.columns = ['ds']
+        date['ds'] = to_datetime(date['ds'])
+        with open('models/Karnataka.pkl', 'rb') as f:
+            model = pickle.load(f)
+        forecast = model.predict(date)
+        x=forecast.iloc[ : ,3].values
+        return redirect(url_for("predict",pred=x))
+
+    else:
+        return render_template("state.html")
+
+@app.route("/westbengal" ,methods=["POST", "GET"])
+def wb():
+    if request.method == "POST":
+        date = list()
+        dat=request.form["nm"]
+        date.append([dat])
+        date = pd.DataFrame(date)
+        date.columns = ['ds']
+        date['ds'] = to_datetime(date['ds'])
+        with open('models/WestBengal.pkl', 'rb') as f:
+            model = pickle.load(f)
+        forecast = model.predict(date)
+        x=forecast.iloc[ : ,3].values
+        return redirect(url_for("predict",pred=x))
+
+    else:
+        return render_template("state.html")
+
+@app.route("/telangana" ,methods=["POST", "GET"])
+def telangana():
+    if request.method == "POST":
+        date = list()
+        dat=request.form["nm"]
+        date.append([dat])
+        date = pd.DataFrame(date)
+        date.columns = ['ds']
+        date['ds'] = to_datetime(date['ds'])
+        with open('models/Telangana.pkl', 'rb') as f:
+            model = pickle.load(f)
+        forecast = model.predict(date)
+        x=forecast.iloc[ : ,3].values
+        return redirect(url_for("predict",pred=x))
+
+    else:
+        return render_template("state.html")
+
+@app.route("/rajasthan" ,methods=["POST", "GET"])
+def rajasth():
+    if request.method == "POST":
+        date = list()
+        dat=request.form["nm"]
+        date.append([dat])
+        date = pd.DataFrame(date)
+        date.columns = ['ds']
+        date['ds'] = to_datetime(date['ds'])
+        with open('models/Rajasthan.pkl', 'rb') as f:
+            model = pickle.load(f)
+        forecast = model.predict(date)
+        x=forecast.iloc[ : ,3].values
+        return redirect(url_for("predict",pred=x))
+
+    else:
+        return render_template("state.html")
+
+@app.route("/maharashtra" ,methods=["POST", "GET"])
+def mh():
+    if request.method == "POST":
+        date = list()
+        dat=request.form["nm"]
+        date.append([dat])
+        date = pd.DataFrame(date)
+        date.columns = ['ds']
+        date['ds'] = to_datetime(date['ds'])
+        with open('models/maharashtra.pkl', 'rb') as f:
+            model = pickle.load(f)
+        forecast = model.predict(date)
+        x=forecast.iloc[ : ,3].values
+        return redirect(url_for("predict",pred=x))
+
+    else:
+        return render_template("state.html")
+
+@app.route("/gujarat" ,methods=["POST", "GET"])
+def gujarat():
+    if request.method == "POST":
+        date = list()
+        dat=request.form["nm"]
+        date.append([dat])
+        date = pd.DataFrame(date)
+        date.columns = ['ds']
+        date['ds'] = to_datetime(date['ds'])
+        with open('models/Gujarat.pkl', 'rb') as f:
+            model = pickle.load(f)
+        forecast = model.predict(date)
+        x=forecast.iloc[ : ,3].values
+        return redirect(url_for("predict",pred=x))
+
+    else:
+        return render_template("state.html")
+
+@app.route("/delhi" ,methods=["POST", "GET"])
+def delhi():
+    if request.method == "POST":
+        date = list()
+        dat=request.form["nm"]
+        date.append([dat])
+        date = pd.DataFrame(date)
+        date.columns = ['ds']
+        date['ds'] = to_datetime(date['ds'])
+        with open('models/Delhi.pkl', 'rb') as f:
+            model = pickle.load(f)
+        forecast = model.predict(date)
+        x=forecast.iloc[ : ,3].values
+        return redirect(url_for("predict",pred=x))
+
+    else:
+        return render_template("state.html")
+
+@app.route("/bihar" ,methods=["POST", "GET"])
+def bihar():
+    if request.method == "POST":
+        date = list()
+        dat=request.form["nm"]
+        date.append([dat])
+        date = pd.DataFrame(date)
+        date.columns = ['ds']
+        date['ds'] = to_datetime(date['ds'])
+        with open('models/Bihar.pkl', 'rb') as f:
+            model = pickle.load(f)
+        forecast = model.predict(date)
+        x=forecast.iloc[ : ,3].values
+        return redirect(url_for("predict",pred=x))
+
+    else:
+        return render_template("state.html")
+
+@app.route("/andhrapradesh" ,methods=["POST", "GET"])
+def ap():
+    if request.method == "POST":
+        date = list()
+        dat=request.form["nm"]
+        date.append([dat])
+        date = pd.DataFrame(date)
+        date.columns = ['ds']
+        date['ds'] = to_datetime(date['ds'])
+        with open('models/AndhraPradesh.pkl', 'rb') as f:
+            model = pickle.load(f)
+        forecast = model.predict(date)
+        x=forecast.iloc[ : ,3].values
+        return redirect(url_for("predict",pred=x))
+
+    else:
+        return render_template("state.html")
+
+@app.route("/goa" ,methods=["POST", "GET"])
+def goa():
+    if request.method == "POST":
+        date = list()
+        dat=request.form["nm"]
+        date.append([dat])
+        date = pd.DataFrame(date)
+        date.columns = ['ds']
+        date['ds'] = to_datetime(date['ds'])
+        with open('models/Goa.pkl', 'rb') as f:
             model = pickle.load(f)
         forecast = model.predict(date)
         x=forecast.iloc[ : ,3].values
