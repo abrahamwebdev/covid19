635335788e7bbd8b42324c8f78703f2fc678f0a8
abraham <agthankachan007@gmail.com>
2021-06-29 10:32:02 +0530

    tn

--- NewWebsite/main_website/app.py
+++ NewWebsite/main_website/app.py
@@ -29,7 +29,25 @@ def kerala():
         return redirect(url_for("predict",pred=x))
 
     else:
-        return render_template("kerala.html")
+        return render_template("state.html")
+
+@app.route("/tn" ,methods=["POST", "GET"])
+def tn():
+    if request.method == "POST":
+        date = list()
+        dat=request.form["nm"]
+        date.append([dat])
+        date = pd.DataFrame(date)
+        date.columns = ['ds']
+        date['ds'] = to_datetime(date['ds'])
+        with open('TamilNadu.pkl', 'rb') as f:
+            model = pickle.load(f)
+        forecast = model.predict(date)
+        x=forecast.iloc[ : ,3].values
+        return redirect(url_for("predict",pred=x))
+
+    else:
+        return render_template("state.html")
 
 @app.route("/<pred>")
 def predict(pred):
